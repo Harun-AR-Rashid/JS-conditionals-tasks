2148f1a620449d442b0cdefed21eab26fbebb641
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-08-31 22:28:22 +0600

    task assign

---
+++ task1.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/ 
+const burger = 500;
+const coke = 30;
+if(burger > 600){
+    console.log("free coke");
+}
+else{
+    console.log("you need", coke);
+}
---
+++ task2.js
@@ -0,0 +1,35 @@
+/*** 
+
+BMI Calculator and Health Category
+
+Create a JavaScript program that calculates the Body Mass Index (BMI) and assigns a health category based on the BMI value. Use nested if-else statements to determine the health category.
+
+    - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
+    - BMI < 18.5, you are underweight.
+    - BMI >= 18.5 and BMI <=24.9, you are normal.
+    - BMI >=25 and BMI <= 29.9, you are overweight.
+    - Otherwise, you are obese.
+
+*/
+const weight = 68;
+const heightInches = 67;
+const heightCm = heightInches * 2.54;
+console.log(heightCm);
+
+
+const heightMeter = heightCm / 100;
+console.log(heightMeter);
+
+const BMI = weight / heightMeter **2
+BMI.toFixed(3);
+const BMIC = parseFloat(BMI.toFixed(3))
+console.log(BMIC);
+if(BMI < 18.5){
+    console.log("You are underweight")
+} else if(BMI >= 18.5 && BMI <=24.9){
+    console.log("You are normal")
+} else if(BMI >=25 && BMI <= 29.9)
+    console.log("You are overweight")
+else{
+    console.log("You are obese")
+}

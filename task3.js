53094a5cbc3ade27686719537ecdc5c4dea9746e
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-01 00:28:23 +0600

    task3 assign

--- task3.js
+++ task3.js
@@ -11,3 +11,18 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+const avgMarks = 87;
+if(avgMarks >= 90 && avgMarks <=100){
+    console.log("You have got A")
+} else if(avgMarks >= 80 && avgMarks <=89){
+    console.log("You have got B")
+} else if(avgMarks >= 70 && avgMarks <=79){
+    console.log("You have got C")
+} else if(avgMarks >= 60 && avgMarks <=69){
+    console.log("You have got D")
+} else if(avgMarks >= 0 && avgMarks <=59){
+    console.log("You have failed")
+}
+else{
+    console.log("You were absent")
+}

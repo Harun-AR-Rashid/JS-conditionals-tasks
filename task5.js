f2c64acb866a3976d95ba60f08df30298846ba0f
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-02 21:20:14 +0600

    ftask done

--- task5.js
+++ task5.js
@@ -14,12 +14,12 @@ const num1 = 54;
 const num2 = 45;
 const answer = num1 * 2;
 const answer2 = num1 + num2;
-// console.log(answer2);
-// console.log(answer); 
-// if(num1 > num2){
-//     console.log(answer);
-// }
-// else{
-//     console.log(answer2);
-// }
+console.log(answer2);
+console.log(answer); 
+if(num1 > num2){
+    console.log(answer);
+}
+else{
+    console.log(answer2);
+}
 num1 > num2 ? console.log(answer) : console.log(answer2);
--- task6.js
+++ task6.js
@@ -4,5 +4,30 @@ Ticket fare Calculator
     - Children (age < 10): free
     - Students get a 50% discount
     - Senior citizens (age >= 60) gets a 15% Discount
-    - Otherwise Regular ticket fare 800 tk
+    - Otherwise Regular tickjet fare 800 tk
 */
+const studentaAge = 50;
+const forTicket = 800;
+// const studentaAge = 08;
+const passenger = "students";
+const totalRent = forTicket * 50/100;
+// console.log(totalRent);
+const seniorSitizen = forTicket * 15/100;
+// console.log(seniorSitizen);
+if(studentaAge <10){
+    console.log("Children's rent is free")
+}
+else{
+    if(studentaAge === passenger){
+        console.log("Your rent is",forTicket - totalRent);
+    }
+    else{
+
+        if(studentaAge >= 60){
+            console.log("Your rent is", forTicket - seniorSitizen)
+        }
+        else{
+            console.log("your rent is", forTicket)
+        }
+    }
+}
